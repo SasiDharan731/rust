d63beae9551714e9e270fcb4fba7ead0a9f5cfa5
SasiDharan731 <dharan731@gmail.com>
2021-11-06 12:05:48 +0530

    Just learnt array basics and string literal

--- src/hello.rs
+++ src/hello.rs
@@ -1,3 +1,8 @@
 pub fn new() {
-    println!("{}", std::u8::MAX)
+    let mut name = String::from("Hi sasi");
+    name.push_str("How are you?");
+    
+    let names: (i8, i8, &str) = (20, 3, "Sasi");
+    
+   print!("{}", names.2);
 }
