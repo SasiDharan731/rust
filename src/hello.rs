83c24591906b99f7c5081dd450a31253f468e0b2
SasiDharan731 <dharan731@gmail.com>
2021-11-06 12:53:44 +0530

    Learnt a simple gussing game from the rust docs

--- src/hello.rs
+++ src/hello.rs
@@ -1,11 +1,12 @@
+use std::io;
 pub fn new() {
-    let b = [10, 20, 30, 5, 3, 9, 10, 50, 88];
-    for nums in b{
-        if nums % 2 == 0 {
-            println!("The number {} is even", nums)
-        }else{
-            println!("The number {} is odd", nums)
-        }
+    let mut input = String::new();
+    println!("Please enter a string value");
+    
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Something went wrong");
 
-    }
+    println!("You have typed {}", input)
+
 }
