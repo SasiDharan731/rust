814266a952dabe9b253a388078cc98ac0723f0ea
SasiDharan731 <dharan731@gmail.com>
2021-11-06 12:18:14 +0530

    Lernt some referencing

--- src/hello.rs
+++ src/hello.rs
@@ -1,8 +1,9 @@
 pub fn new() {
-    let mut name = String::from("Hi sasi");
-    name.push_str("How are you?");
-    
-    let names: (i8, i8, &str) = (20, 3, "Sasi");
-    
-   print!("{}", names.2);
+    let b = [10, 20, 30];
+
+    let a = b;
+
+    println!(
+        "{:?}", a
+    )
 }
