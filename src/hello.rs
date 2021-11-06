f12a82138a952f41c1e90c45fbfccc2f14d013a2
SasiDharan731 <dharan731@gmail.com>
2021-11-06 12:24:12 +0530

    Learn 'if' statement

--- src/hello.rs
+++ src/hello.rs
@@ -1,9 +1,11 @@
 pub fn new() {
-    let b = [10, 20, 30];
+    let b = [10, 20, 30, 5, 3, 9, 10, 50, 88];
+    for nums in b{
+        if nums % 2 == 0 {
+            println!("The number {} is even", nums)
+        }else{
+            println!("The number {} is odd", nums)
+        }
 
-    let a = b;
-
-    println!(
-        "{:?}", a
-    )
+    }
 }
